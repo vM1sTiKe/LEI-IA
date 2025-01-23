537a87da3b186f9e80f9c023b892b43a23e725f7
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-01-23 13:36:46 +0000

    Finished memoization
    Memoization is not something that can always be used, because it easily runs out of memory (if board is big)
    So added verification is if to use or not memoization
    
    TODO:
    Max time (analyse and get the depth from the time given)
    Statistics: cada jogada, qual a posicao, quantos nos analisados, quantos cortes alpha e beta tempo gasto e tempo permitido, valor retornado pelo algoritmo

--- p2/lisp/algorithm.lisp
+++ p2/lisp/algorithm.lisp
@@ -3,21 +3,66 @@
 (defparameter *spawner* NIL)
 (defparameter *heuristic* NIL)
 (defparameter *terminal* NIL)
+(defparameter *memoization-usage* NIL)
 
-(defparameter *algorithm-hash* (make-hash-table))
+(defparameter *algorithm-hashtable* NIL)
 
 
 (DEFUN set-spawner (method)
+  "
+    Arguments
+      - method (Function)
+    
+    Sets the method to generade nodes from one node
+  "
   (setf *spawner* method)
 )
 (DEFUN set-heuristic (method)
+  "
+    Arguments
+      - method (Function)
+
+    Sets the method to be called to calculate the node heuristic/real value
+  "
   (setf *heuristic* method)
 )
 (DEFUN set-terminal (method)
+  "
+    Arguments
+      - method (Function)
+    
+    Sets the method to be called to verify is is a terminal node
+  "
   (setf *terminal* method)
 )
+(DEFUN set-memoization-usage (boolean)
+  "
+    Arguments
+      - boolean (Boolean)
 
-(DEFUN execute (spawner heuristic terminal node depth)
+    Defines if is to use memoization on the algorithm
+  "
+  (setf *memoization-usage* boolean)
+)
+(DEFUN set-hashtable ()
+  "
+    Method to create empty hashtable
+  "
+  (setf *algorithm-hashtable* (make-hash-table))
+)
+(DEFUN sethash (key hashtable value)
+  "
+    Arguments
+      - key (Key)
+      - hastable (Hash-table)
+      - value (any)
+    
+    Method to add to the hashtable the pair key:value
+  "
+  (setf (gethash key *algorithm-hashtable*) value)
+)
+
+(DEFUN execute (spawner heuristic terminal node depth &optional (use-memoization NIL))
   "
     Arguments:
       - spawner (method): Nodes spawner method, this method has one in param and it is the parent node to generate the childs from;
@@ -35,13 +80,18 @@
     (set-spawner spawner)
     (set-heuristic heuristic)
     (set-terminal terminal)
+    (set-memoization-usage use-memoization)
+    (set-hashtable)
     (LET (
         (evaluation (core node depth)) ;Execute algorithm
       )
       (PROGN
-        ; (set-spawner NIL)
-        ; (set-heuristic NIL)
-        ; (set-terminal NIL)
+        (set-spawner NIL)
+        (set-heuristic NIL)
+        (set-terminal NIL)
+        (set-memoization-usage NIL)
+        (set-hashtable)
+
         ; evaluation
         (FORMAT T "~a~%" evaluation)
         (NTH 0 evaluation)
@@ -60,15 +110,14 @@
 
     The use of this method makes the algorithm slower... but its asked to exist.
     No sorting: 1:34 a full game between 2 AIs, on a full 8 board game.
-    Com sorting: 2:28
+    With sorting: 2:28
   "
   (SORT (FUNCALL *spawner* node) (IF is-max-player #'< #'>) :key *heuristic*)
-  ; (FUNCALL *spawner* node)
 )
 
 (DEFUN core (node depth &optional (alpha most-negative-fixnum) (beta most-positive-fixnum) (is-max-player T))
-  (IF (gethash node *algorithm-hash*) ;Verifies if the node is cached
-    (LIST node (gethash node *algorithm-hash*)) ;Return if cached
+  (IF (gethash node *algorithm-hashtable*) ;Verifies if the node is cached
+    (LIST node (gethash node *algorithm-hashtable*)) ;Return if cached
     (COND ;Execute normaly to find the node value
       ((OR (ZEROP depth) (FUNCALL *terminal* node) (NULL (FUNCALL *spawner* node))) ;If one of the end conditions
         (LET (
@@ -76,7 +125,7 @@
             (depth-calc (IF (FUNCALL *terminal* node) depth 0)) ;If a node is terminal give him a weight using the depth.
           ) ; The if validating the is-max-player is to inver the values, meaning if the node evaluation of a min node is negative to THAT PLAYER, means it positive to the max player, needing to flip signal
           (PROGN
-            ; (setf (gethash node *algorithm-hash*) heuristic-value) ;Link heuristic value to node on the hash-table
+            (IF *memoization-usage* (sethash node *algorithm-hashtable* heuristic-value)) ;Link heuristic value to node on the hash-table (if is to use memoization)
             (LIST node (* (IF is-max-player 1 -1) (1+ depth-calc) heuristic-value)) ;Depth-calc will be the weight or 0, simulating it was found on the last depth, it cannot be 0 because multiplication so increment 1
           )
         )
